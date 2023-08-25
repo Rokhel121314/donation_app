4e3564a12f5b3416803fef6713a8b9c9283d819d
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-08-25 12:31:15 +0800

    implement firebase registration successfuly

---
+++ src/api/firebase/user.ts
@@ -0,0 +1,23 @@
+import auth from '@react-native-firebase/auth';
+
+export const createUser = async (
+  fullName: string,
+  email: string,
+  password: string,
+) => {
+  try {
+    const user = await auth().createUserWithEmailAndPassword(email, password);
+    await user.user.updateProfile({displayName: fullName});
+    console.log('user', user);
+  } catch (error: any) {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  }
+};
--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,5 +1,5 @@
 import {View, Text, SafeAreaView, ScrollView, StatusBar} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import globalStyle from '../../../assets/styles/globalStyle';
 import style from './style';
 import BackButton from '../../components/BackButton/BackButton';
@@ -7,8 +7,16 @@ import {RootStackScreenProps} from '../../navigation/navigationType';
 import Header from '../../components/Header/Header';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
+import {createUser} from '../../api/firebase/user';
 
 const Register = ({navigation}: RootStackScreenProps<'Register'>) => {
+  //
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  console.log('detail', fullName, email, password);
+
   return (
     <SafeAreaView style={[globalStyle.bgWhite, globalStyle.flex]}>
       <StatusBar barStyle={'dark-content'} />
@@ -24,6 +32,7 @@ const Register = ({navigation}: RootStackScreenProps<'Register'>) => {
             <Input
               label={'First & Last Name'}
               placeHolder={'Enter your Full Name..'}
+              onChangeText={text => setFullName(text)}
             />
           </View>
 
@@ -32,6 +41,7 @@ const Register = ({navigation}: RootStackScreenProps<'Register'>) => {
               label={'Email'}
               keyboardType={'email-address'}
               placeHolder={'youremail@gmail.com'}
+              onChangeText={text => setEmail(text)}
             />
           </View>
 
@@ -40,14 +50,14 @@ const Register = ({navigation}: RootStackScreenProps<'Register'>) => {
               label={'Password'}
               placeHolder="******"
               secureTextEntry={true}
+              onChangeText={text => setPassword(text)}
             />
           </View>
           <View style={globalStyle.marginBottom24}>
             <Button
               title="Register"
-              onPress={() => {
-                {
-                }
+              onPress={async () => {
+                await createUser(fullName, email, password);
               }}
             />
           </View>
